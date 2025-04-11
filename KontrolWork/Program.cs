17029fd4e691f6a5b5412d981336e44f10cc05dd
Alinafrolova05 <145061624+Alinafrolova05@users.noreply.github.com>
2025-04-11 15:18:49 +0300

    feat:

---
+++ KontrolWork/Program.cs
@@ -0,0 +1,6 @@
+﻿using KontrolWork;
+
+Queue queue = new();
+
+queue.Enqueue(1);
+queue.Enqueue(2);
---
+++ KontrolWork/Test.cs
@@ -0,0 +1,39 @@
+﻿using KontrolWork;
+using NUnit.Framework;
+class Test
+{
+    private Queue queue;
+
+    [SetUp]
+    public void Setup()
+    {
+        queue = new();
+    }
+
+    [Test]
+    public void SimpleTest()
+    {
+        for (var i = 0; i < 10; ++i)
+        {
+            queue.Enqueue(i);
+        }
+        queue.Enqueue(10);
+        queue.Enqueue(11);
+
+        queue.Dequeue();
+        queue.Dequeue();
+
+        int[] checkArray = { 2, 3, 4, 5, 6, 7, 8, 9, 10, 11 };
+
+        for (var i = 0; i < checkArray.Length; ++i)
+        {
+            Assert.AreEqual(checkArray[i], queue.Dequeue());
+        }
+    }
+
+    [Test]
+    public void ExceptionTest()
+    {
+        Assert.Throws<InvalidOperationException>(() => queue.Dequeue());
+    }
+}
