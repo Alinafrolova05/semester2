98ad67dda0faea3fd0187dc102eb0f00a8f04301
Alinafrolova05 <145061624+Alinafrolova05@users.noreply.github.com>
2025-05-15 00:33:15 +0300

    feat: add solution

---
+++ MyLinq/MyClass.cs
@@ -0,0 +1,73 @@
+﻿namespace MyLinq;
+
+/// <summary>
+/// 
+/// </summary>
+public static class MyClass
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <returns></returns>
+    public static IEnumerable<int> GetPrimes()
+    {
+        for (var i = 1; true; i++)
+        {
+            bool isPrime = true;
+            for (var j = 2; i >= j * j; ++j)
+            {
+                if (i % j == 0)
+                {
+                    isPrime = false;
+                    break;
+                }
+            }
+
+            if (isPrime)
+            {
+                yield return i;
+            }
+        }
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="seq"></param>
+    /// <param name="n"></param>
+    /// <returns></returns>
+    public static IEnumerable<int> Take(this IEnumerable<int> seq, int n)
+    {
+        int count = 1;
+        foreach (var i in seq)
+        {
+            if (count > n)
+            {
+                break;
+            }
+
+            count++;
+            yield return i;
+        }
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="seq"></param>
+    /// <param name="n"></param>
+    /// <returns></returns>
+    public static IEnumerable<int> Skip(this IEnumerable<int> seq, int n)
+    {
+        int count = 0;
+        foreach (var i in seq)
+        {
+            if (count > n)
+            {
+                yield return i;
+            }
+
+            count++;
+        }
+    }
+}
---
+++ MyLinq/Program.cs
@@ -0,0 +1,8 @@
+﻿using MyLinq;
+
+IEnumerable<int> seq = MyClass.GetPrimes().Take(5);
+
+foreach (var i in seq)
+{
+    Console.WriteLine(i);
+}
